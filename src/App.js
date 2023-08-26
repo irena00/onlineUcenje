d590074099692c612ced6d9011665c0243fa67d8
irena00 <randjeloviceva13@gmail.com>
2023-08-26 22:37:10 +0200

    testiranje

--- src/App.js
+++ src/App.js
@@ -7,16 +7,26 @@ import Navbar from './Navbar';
 import Courses from './Courses';
 import Course from './Course';
 import Test from './Test';
- 
+import Profesor from './Profesor';
+import ProfesorKurs from './ProfesorKurs';
+import Update from './Update';
+import Insert from './Insert';
+import HomePage from './HomePage';
+
 function App() {
   return (
     <Router>
         
         <Navbar></Navbar>
         <Routes>
-        
-          
-          <Route path="/" element={   <Login></Login>} />
+        <Route path="/insert/:id" element={   <Insert></Insert>} />
+         
+        <Route path="/update/:id" element={   <Update></Update>} />
+
+        <Route path="/profesor" element={   <Profesor></Profesor>} />
+        <Route path="/profesor/kursevi/:id" element={   <ProfesorKurs></ProfesorKurs>} />
+        <Route path="/" element={   <HomePage></HomePage>} />
+          <Route path="/login" element={   <Login></Login>} />
           <Route path="/register" element={   <Register></Register>} />
           <Route path="/kursevi" element={   <Courses></Courses>} />
           <Route path="/kursevi/:id" element={   <Course></Course>} />
