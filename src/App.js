2795897904ab6b0c0deb70738ed6fe7f3610667e
irena00 <randjeloviceva13@gmail.com>
2023-08-26 22:31:18 +0200

    dodate rute na frontu

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
  
 import './App.css';
-
+import Login from './Login';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Register from './Register';
+import Navbar from './Navbar';
+import Courses from './Courses';
+import Course from './Course';
+import Test from './Test';
+ 
 function App() {
   return (
-    <div className="App">
-       
-    </div>
+    <Router>
+        
+        <Navbar></Navbar>
+        <Routes>
+        
+          
+          <Route path="/" element={   <Login></Login>} />
+          <Route path="/register" element={   <Register></Register>} />
+          <Route path="/kursevi" element={   <Courses></Courses>} />
+          <Route path="/kursevi/:id" element={   <Course></Course>} />
+          <Route path="/test/:id" element={   <Test></Test>} />
+
+          
+        </Routes>
+        
+    </Router>
+ 
   );
 }
 
